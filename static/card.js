7c38dd25137ff9cf699a24846caa9f28a502df4c
Nidaaa <nidashaikh16@eng.rizvi.edu.in>
2024-03-24 06:20:09 +0530

    firebase issue fixed

--- static/card.js
+++ static/card.js
@@ -1,20 +1,30 @@
+const data = [{
+    project_name: "Project 1",
+    property_type: "Apartment",
+    area_in_sqft: 1000,
+    developer_name: "Developer A",
+    bedrooms: 2,
+    bathrooms: 2,
+    location: "Bandra",
+    project_status: "Pre-launch"
+},
+{
+    project_name: "Project 2",
+    property_type: "Villa",
+    area_in_sqft: 1500,
+    developer_name: "Developer B",
+    bedrooms: 3,
+    bathrooms: 3,
+    location: "Andheri",
+    project_status: "Under Construction"
+},
+// Add more data as needed
+];
 // Function to create cards
 function createCards(data) {
     const container = document.getElementById("cardContainer");
     let row;
 
-    // Your web app's Firebase configuration
-    const usersRef = fetch("https://trial-b8c5a-default-rtdb.firebaseio.com", {
-        method: "POST",
-        body: JSON.stringify({
-            apiKey: 'AIzaSyAMd6Tw1a0k6F1CG4al43Vk6CRLCFlpy_4',
-        }),
-        headers: {
-            "Content-type": "application/json"
-        }
-    });
-
-    console.log(usersRef)
 
     data.forEach((item, index) => {
         if (index % 4 === 0) {
